ebbbddfe1f3e66f24c51100db5c1ff1dd25d35ae
lumin <luminovoez@gmail.com>
2026-06-01 13:29:00 +0800

    remove navbar; canvas takes fullscreen

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
-import { Nav } from './Nav';
 import { useHashRoute } from './router';
 import { SingleDisplay } from './SingleDisplay';
 import { MultipleDisplay } from './MultipleDisplay';
 
 export default function App() {
   const route = useHashRoute();
-  return (
-    <>
-      <Nav route={route} />
-      {route === 'single' ? <SingleDisplay /> : <MultipleDisplay />}
-    </>
-  );
+  return route === 'single' ? <SingleDisplay /> : <MultipleDisplay />;
 }
