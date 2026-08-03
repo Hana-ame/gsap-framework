0030dd95aed3225b9fddf96090ce8fe567d1b632
lumin <luminovoez@gmail.com>
2026-08-04 05:03:20 +0800

    translate(vn): isekai HC21/HC31 to Chinese

--- src/vn/scenes/isekai_HC21_36.ts
+++ src/vn/scenes/isekai_HC21_36.ts
@@ -44,193 +44,193 @@ export const isekai_HC21_36: VnScript = {
  {
   "type": "say",
   "speaker": "教皇マルクス",
-  "text": "そうやってお尻を突き出すだけでパンツが見えますね"
- },
- {
-  "type": "say",
-  "speaker": "教皇マルクス",
-  "text": "やはり丈の短い服にして正解でしたね"
- },
- {
-  "type": "say",
-  "speaker": "詩保",
-  "text": "教皇様が喜んでくれるのでしたら短くして良かったです"
- },
- {
-  "type": "cg",
-  "key": "HC21-2"
- },
- {
-  "type": "cg",
-  "key": "HC21-2"
- },
- {
-  "type": "say",
-  "speaker": "詩保",
-  "text": "さあ教皇様\n早く私の中に入れてください"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "期待で愛液が溢れてくる詩保。"
- },
- {
-  "type": "say",
-  "speaker": "教皇マルクス",
-  "text": "では入れますね"
- },
- {
-  "type": "say",
-  "speaker": "詩保",
-  "text": "はい、来てください教皇様"
- },
- {
-  "type": "cg",
-  "key": "HC21-3"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "ズブリと教皇のペニスが詩保の中に挿入される。"
- },
- {
-  "type": "say",
-  "speaker": "詩保",
-  "text": "ああん、教皇様のが入ってくる……"
- },
- {
-  "type": "say",
-  "speaker": "教皇マルクス",
-  "text": "毎日のようにやっているのにあなたの中はまだまだキツイですね"
- },
- {
-  "type": "say",
-  "speaker": "教皇マルクス",
-  "text": "よほど私のペニスを離したくないようですね"
- },
- {
-  "type": "say",
-  "speaker": "詩保",
-  "text": "はい、ずっとこうしていたいぐらいです"
- },
- {
-  "type": "cg",
-  "key": "HC21-4"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "動き出す教皇。"
- },
- {
-  "type": "say",
-  "speaker": "詩保",
-  "text": "ああんっ教皇様気持ち良いですぅ"
- },
- {
-  "type": "say",
-  "speaker": "教皇マルクス",
-  "text": "私も気持ちいですよ"
- },
- {
-  "type": "say",
-  "speaker": "詩保",
-  "text": "嬉しいです教皇様\nもっと激しく突いてください"
- },
- {
-  "type": "say",
-  "speaker": "教皇マルクス",
-  "text": "いいでしょう"
- },
- {
-  "type": "cg",
-  "key": "HC21-5"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "パンパンと腰を打ち付ける音が響く。"
- },
- {
-  "type": "say",
-  "speaker": "詩保",
-  "text": "ああんっ！これ好きですぅ！"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "詩保は快楽に溺れていた。"
- },
- {
-  "type": "say",
-  "speaker": "教皇マルクス",
-  "text": "そろそろ出そうです"
- },
- {
-  "type": "say",
-  "speaker": "詩保",
-  "text": "はい教皇様、私の{膣内|なか}にたくさん出してください"
- },
- {
-  "type": "say",
-  "speaker": "教皇マルクス",
-  "text": "ああ出ますっ！"
- },
- {
-  "type": "cg",
-  "key": "HC21-6"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "",
-  "effect": "flash"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "",
-  "effect": "flash"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "教皇は詩保の中に射精した。同時に詩保も絶頂する。"
- },
- {
-  "type": "cg",
-  "key": "HC21-7"
- },
- {
-  "type": "say",
-  "speaker": "詩保",
-  "text": "ああ……熱い……"
- },
- {
-  "type": "say",
-  "speaker": "教皇マルクス",
-  "text": "ふう、どうでしたか？"
- },
- {
-  "type": "say",
-  "speaker": "詩保",
-  "text": "はいぃ、とても気持ち良かったです…"
- },
- {
-  "type": "say",
-  "speaker": "教皇マルクス",
-  "text": "まだまだ相手をしてもらいますよ"
- },
- {
-  "type": "say",
-  "speaker": "詩保",
-  "text": "はい、もっと私で気持ち良くなってください"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "こうして教皇と何度も交わる詩保だった…"
- },
+   "text": "像这样把屁股一翘，内裤就露出来了呢"
+  },
+  {
+   "type": "say",
+   "speaker": "教皇马尔克斯",
+   "text": "果然把衣服做短是正确的"
+  },
+  {
+   "type": "say",
+   "speaker": "诗保",
+   "text": "只要教皇大人高兴，做短就值得了"
+  },
+  {
+   "type": "cg",
+   "key": "HC21-2"
+  },
+  {
+   "type": "cg",
+   "key": "HC21-2"
+  },
+  {
+   "type": "say",
+   "speaker": "诗保",
+   "text": "来吧，教皇大人\n快点进入我的身体吧"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "因为期待，爱液从诗保身上溢了出来。"
+  },
+  {
+   "type": "say",
+   "speaker": "教皇马尔克斯",
+   "text": "那我进去了"
+  },
+  {
+   "type": "say",
+   "speaker": "诗保",
+   "text": "是，请来吧，教皇大人"
+  },
+  {
+   "type": "cg",
+   "key": "HC21-3"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "教皇的阴茎咕噜一下插入了诗保的体内。"
+  },
+  {
+   "type": "say",
+   "speaker": "诗保",
+   "text": "啊啊嗯，教皇大人的进来了……"
+  },
+  {
+   "type": "say",
+   "speaker": "教皇马尔克斯",
+   "text": "明明几乎每天都在做，你的里面还是这么紧呢"
+  },
+  {
+   "type": "say",
+   "speaker": "教皇马尔克斯",
+   "text": "看来你很不愿意让我的阴茎离开呢"
+  },
+  {
+   "type": "say",
+   "speaker": "诗保",
+   "text": "是，真想一直这样下去"
+  },
+  {
+   "type": "cg",
+   "key": "HC21-4"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "教皇开始动了起来。"
+  },
+  {
+   "type": "say",
+   "speaker": "诗保",
+   "text": "啊啊嗯，教皇大人好舒服"
+  },
+  {
+   "type": "say",
+   "speaker": "教皇马尔克斯",
+   "text": "我也很舒服哦"
+  },
+  {
+   "type": "say",
+   "speaker": "诗保",
+   "text": "太高兴了，教皇大人\n请更激烈地顶进来"
+  },
+  {
+   "type": "say",
+   "speaker": "教皇马尔克斯",
+   "text": "好吧"
+  },
+  {
+   "type": "cg",
+   "key": "HC21-5"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "啪、啪的腰部撞击声回响着。"
+  },
+  {
+   "type": "say",
+   "speaker": "诗保",
+   "text": "啊啊嗯！我好喜欢这样！"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "诗保沉溺在快感之中。"
+  },
+  {
+   "type": "say",
+   "speaker": "教皇马尔克斯",
+   "text": "差不多要射了"
+  },
+  {
+   "type": "say",
+   "speaker": "诗保",
+   "text": "是，教皇大人，请把精液大量射进我的{身体|里面}"
+  },
+  {
+   "type": "say",
+   "speaker": "教皇马尔克斯",
+   "text": "啊啊，要射了！"
+  },
+  {
+   "type": "cg",
+   "key": "HC21-6"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "",
+   "effect": "flash"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "",
+   "effect": "flash"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "教皇射在了诗保体内。同时，诗保也到达了高潮。"
+  },
+  {
+   "type": "cg",
+   "key": "HC21-7"
+  },
+  {
+   "type": "say",
+   "speaker": "诗保",
+   "text": "啊啊……好烫……"
+  },
+  {
+   "type": "say",
+   "speaker": "教皇马尔克斯",
+   "text": "呼，感觉怎么样？"
+  },
+  {
+   "type": "say",
+   "speaker": "诗保",
+   "text": "是，非常舒服……"
+  },
+  {
+   "type": "say",
+   "speaker": "教皇马尔克斯",
+   "text": "以后还会继续陪你玩的哦"
+  },
+  {
+   "type": "say",
+   "speaker": "诗保",
+   "text": "是，请用我获得更多快乐吧"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "就这样，诗保一次又一次地与教皇交合着…"
+  },
  {
   "type": "end",
   "goto": "#vn-menu"
--- src/vn/scenes/isekai_HC31_37.ts
+++ src/vn/scenes/isekai_HC31_37.ts
@@ -64,290 +64,290 @@ export const isekai_HC31_37: VnScript = {
  {
   "type": "say",
   "speaker": "有理紗",
-  "text": "私も詩保みたいに洗脳するの？"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "詩保に敗北した後、拘束された有理紗。"
- },
- {
-  "type": "say",
-  "speaker": "教皇マルクス",
-  "text": "ええ、ですがそれは今ではありません"
- },
- {
-  "type": "say",
-  "speaker": "教皇マルクス",
-  "text": "あなたにはまだ抵抗してもらわないといけない理由がありますから"
- },
- {
-  "type": "say",
-  "speaker": "教皇マルクス",
-  "text": "ですが、そんな抵抗できない姿を見せられると興奮してきますね"
- },
- {
-  "type": "say",
-  "speaker": "教皇マルクス",
-  "text": "少し私の性処理に付き合ってもらいましょう"
- },
- {
-  "type": "cg",
-  "key": "HC31-2"
- },
- {
-  "type": "cg",
-  "key": "HC31-2"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "そう言い勃起したペニスを見せ付けて来る教皇。"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "嫌、来ないで！"
- },
- {
-  "type": "say",
-  "speaker": "教皇マルクス",
-  "text": "嫌がる女性とするのは初めてですね\n皆さん喜んで私に身体を差し出してくれますから"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "それはあなたが洗脳してるからでしょ！"
- },
- {
-  "type": "say",
-  "speaker": "教皇マルクス",
-  "text": "ええその通りです\nですから新鮮な気持ちですよ"
- },
- {
-  "type": "say",
-  "speaker": "教皇マルクス",
-  "text": "さあ、入れますよ！"
- },
- {
-  "type": "cg",
-  "key": "HC31-3"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "",
-  "effect": "flash"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "教皇のペニスが有理紗の中に挿入される。"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "痛い！痛いいいい"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "初めての痛みもあり叫ぶ有理紗。"
- },
- {
-  "type": "say",
-  "speaker": "教皇マルクス",
-  "text": "初めてだったのですか\nまあ私には関係ありませんが"
- },
- {
-  "type": "cg",
-  "key": "HC31-4"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "そう言い腰を動かし出す教皇。"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "ううっ"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "痛みに耐える有理紗。"
- },
- {
-  "type": "say",
-  "speaker": "教皇マルクス",
-  "text": "ああ、とても気持ち良いですよ"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "有理紗のことなど気にせず自分だけ気持ち良くなる教皇。"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "パンパンと腰を打ち付ける音が響く。"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "痛い…もうやめて…"
- },
- {
-  "type": "say",
-  "speaker": "教皇マルクス",
-  "text": "やめるわけないでしょう\nもっと激しく動きますよ"
- },
- {
-  "type": "cg",
-  "key": "HC31-5"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "いやああっ！！"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "さらに早くなる腰の動き。"
- },
- {
-  "type": "say",
-  "speaker": "教皇マルクス",
-  "text": "ああ、出そうですよ！"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "いやいやいやあああああ"
- },
- {
-  "type": "cg",
-  "key": "HC31-6"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "",
-  "effect": "flash"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "ドピュッ！ビュルルルー！！ドクンドクン……"
- },
- {
-  "type": "cg",
-  "key": "HC31-7"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "中に出される精液の感触を感じる有理紗。"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "もう嫌…"
- },
- {
-  "type": "say",
-  "speaker": "詩保",
-  "text": "教皇様、準備が整いました"
- },
- {
-  "type": "say",
-  "speaker": "教皇マルクス",
-  "text": "おっそうですか"
- },
- {
-  "type": "cg",
-  "key": "HC31-8"
- },
- {
-  "type": "say",
-  "speaker": "教皇マルクス",
-  "text": "良かったですね有理紗さん\nこれであたなも私の信徒になれますよ"
- },
- {
-  "type": "cg",
-  "key": "HC31-9"
- },
- {
-  "type": "cg",
-  "key": "HC31-9"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "有理紗に手をかざす教皇。"
- },
- {
-  "type": "cg",
-  "key": "HC31-10"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "そう言いながら洗脳魔法を発動する教皇。"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "(ああ･･･だめぇ･･･頭が･･･ボーっとしてきて･･･)"
- },
- {
-  "type": "cg",
-  "key": "HC31-11"
- },
- {
-  "type": "cg",
-  "key": "HC31-11"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "(でもこれで私も･･･詩保と同じ･･･教皇様のものに･･･)"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "(教皇様のものに･･･幸せ･･････)"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "無理やり犯され心が弱っている有理紗は\n抵抗する力もなくあっけなく教皇の洗脳魔法に堕ちる…"
- },
- {
-  "type": "cg",
-  "key": "HC31-12"
- },
- {
-  "type": "cg",
-  "key": "HC31-12"
- },
- {
-  "type": "say",
-  "speaker": "教皇マルクス",
-  "text": "さて･･･次はあの方ですね･･･"
- },
- {
-  "type": "say",
-  "speaker": "教皇マルクス",
-  "text": "有理紗さんも来るのですよ"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "はい、教皇様"
- },
+   "text": "你也要像诗保那样给我洗脑吗？"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "败给诗保之后，有理纱被束缚住了。"
+  },
+  {
+   "type": "say",
+   "speaker": "教皇马尔克斯",
+   "text": "是的，但还不是现在"
+  },
+  {
+   "type": "say",
+   "speaker": "教皇马尔克斯",
+   "text": "你还有必须继续抵抗的理由"
+  },
+  {
+   "type": "say",
+   "speaker": "教皇马尔克斯",
+   "text": "不过，看到你无法反抗的样子，我兴奋起来了"
+  },
+  {
+   "type": "say",
+   "speaker": "教皇马尔克斯",
+   "text": "来陪我解决一下性欲吧"
+  },
+  {
+   "type": "cg",
+   "key": "HC31-2"
+  },
+  {
+   "type": "cg",
+   "key": "HC31-2"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "说着，教皇露出了勃起的阴茎。"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "不要，别过来！"
+  },
+  {
+   "type": "say",
+   "speaker": "教皇马尔克斯",
+   "text": "和抗拒的女性做还是第一次\n大家都心甘情愿地向我献出身体"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "那是因为你给她们洗脑了吧！"
+  },
+  {
+   "type": "say",
+   "speaker": "教皇马尔克斯",
+   "text": "没错，正是如此\n所以感觉很新鲜"
+  },
+  {
+   "type": "say",
+   "speaker": "教皇马尔克斯",
+   "text": "来吧，我要进去了！"
+  },
+  {
+   "type": "cg",
+   "key": "HC31-3"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "",
+   "effect": "flash"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "教皇的阴茎插入了有理纱的体内。"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "好痛！好痛啊啊啊"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "因为是第一次的疼痛，有理纱叫了出来。"
+  },
+  {
+   "type": "say",
+   "speaker": "教皇马尔克斯",
+   "text": "原来是第一次啊\n不过与我无关"
+  },
+  {
+   "type": "cg",
+   "key": "HC31-4"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "说着，教皇开始扭动腰部。"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "呜呜"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "有理纱忍受着疼痛。"
+  },
+  {
+   "type": "say",
+   "speaker": "教皇马尔克斯",
+   "text": "啊啊，非常舒服"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "教皇完全不顾有理纱，只顾自己享受。"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "啪、啪的腰部撞击声回响着。"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "好痛…住手…"
+  },
+  {
+   "type": "say",
+   "speaker": "教皇马尔克斯",
+   "text": "怎么可能停\n我要动得更激烈"
+  },
+  {
+   "type": "cg",
+   "key": "HC31-5"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "不要啊啊！！"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "腰部动作更加快了。"
+  },
+  {
+   "type": "say",
+   "speaker": "教皇马尔克斯",
+   "text": "啊啊，要射了！"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "不要不要不要啊啊啊啊"
+  },
+  {
+   "type": "cg",
+   "key": "HC31-6"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "",
+   "effect": "flash"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "噗——！哔噜噜噜——！！咕嘟咕嘟……"
+  },
+  {
+   "type": "cg",
+   "key": "HC31-7"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "有理纱感受到了精液射进体内的触感。"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "讨厌死了…"
+  },
+  {
+   "type": "say",
+   "speaker": "诗保",
+   "text": "教皇大人，准备做好了"
+  },
+  {
+   "type": "say",
+   "speaker": "教皇马尔克斯",
+   "text": "哦，是吗"
+  },
+  {
+   "type": "cg",
+   "key": "HC31-8"
+  },
+  {
+   "type": "say",
+   "speaker": "教皇马尔克斯",
+   "text": "太好了呢，有理纱小姐\n这样你也能成为我的信徒了"
+  },
+  {
+   "type": "cg",
+   "key": "HC31-9"
+  },
+  {
+   "type": "cg",
+   "key": "HC31-9"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "教皇把手伸向有理纱。"
+  },
+  {
+   "type": "cg",
+   "key": "HC31-10"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "说着，教皇发动了洗脑魔法。"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "（啊啊…不行…脑袋…开始变得昏沉…）"
+  },
+  {
+   "type": "cg",
+   "key": "HC31-11"
+  },
+  {
+   "type": "cg",
+   "key": "HC31-11"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "（但这样一来，我也能…和诗保一样…成为教皇大人的…）"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "（成为教皇大人的…幸福……）"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "被强行侵犯、内心脆弱的理纱\n毫无抵抗力地轻易坠入了教皇的洗脑魔法…"
+  },
+  {
+   "type": "cg",
+   "key": "HC31-12"
+  },
+  {
+   "type": "cg",
+   "key": "HC31-12"
+  },
+  {
+   "type": "say",
+   "speaker": "教皇马尔克斯",
+   "text": "那么…下一个是那位大人了…"
+  },
+  {
+   "type": "say",
+   "speaker": "教皇马尔克斯",
+   "text": "有理纱小姐也来吧"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "是，教皇大人"
+  },
  {
   "type": "end",
   "goto": "#vn-menu"
