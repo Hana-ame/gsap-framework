fc44f3f63dde6f321b520a1c07d14f103eee7901
lumin <luminovoez@gmail.com>
2026-08-04 04:49:58 +0800

    translate(vn): isekai HC12 to Chinese

--- src/vn/scenes/isekai_HC12_35.ts
+++ src/vn/scenes/isekai_HC12_35.ts
@@ -44,200 +44,200 @@ export const isekai_HC12_35: VnScript = {
  {
   "type": "say",
   "speaker": "小茂田",
-  "text": "ほらおいで"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "椅子に腰かけ勃起したペニスを見せて来る小茂田。"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "では入れますね"
- },
- {
-  "type": "cg",
-  "key": "HC12-2"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "",
-  "effect": "flash"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "そう言い、小茂田に抱きつく形で挿入する。"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "ああ…これが小茂田様の…"
- },
- {
-  "type": "say",
-  "speaker": "小茂田",
-  "text": "どうだい？チンポ入れらた気分は？"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "小茂田様を感じることができてとても幸せです"
- },
- {
-  "type": "say",
-  "speaker": "小茂田",
-  "text": "それは良かったよ、じゃあ動こうか"
- },
- {
-  "type": "cg",
-  "key": "HC12-3"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "パンパンパンパン"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "あん……ああん……"
- },
- {
-  "type": "say",
-  "speaker": "小茂田",
-  "text": "僕の女になれてどんな気分だい？"
- },
- {
-  "type": "cg",
-  "key": "HC12-4"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "はい、とても幸せですうう♡"
- },
- {
-  "type": "say",
-  "speaker": "小茂田",
-  "text": "これからも僕の女でいてくれるかい？"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "もちろんですぅ♡\n有理紗は永遠に小茂田様のものですぅ♡"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "あひぃ……いいっ！もっと、もっと激しくしてください"
- },
- {
-  "type": "say",
-  "speaker": "小茂田",
-  "text": "わかったよ\nほらこれでどうだい"
- },
- {
-  "type": "cg",
-  "key": "HC12-5"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "動きが激しくなる。"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "ああんっ気持ち良いです、小茂田様ぁ、愛してますぅ"
- },
- {
-  "type": "say",
-  "speaker": "小茂田",
-  "text": "僕も有理紗のこと愛してるよ"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "嬉しいです小茂田様！"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "小茂田様小茂田様ぁ！私もう…イっちゃいそうです…"
- },
- {
-  "type": "say",
-  "speaker": "小茂田",
-  "text": "僕もそろそろ出そうだよ･･･"
- },
- {
-  "type": "say",
-  "speaker": "小茂田",
-  "text": "ああっ出るっ"
- },
- {
-  "type": "cg",
-  "key": "HC12-6"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "",
-  "effect": "flash"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "",
-  "effect": "flash"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "ドピュドピュッ"
- },
- {
-  "type": "cg",
-  "key": "HC12-7"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "ああぁ、小茂田様の精液が私の中に…"
- },
- {
-  "type": "say",
-  "speaker": "小茂田",
-  "text": "ふう……よかったよ有理紗ちゃん……"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "ありがとうございます小茂田様♡"
- },
- {
-  "type": "say",
-  "speaker": "小茂田",
-  "text": "これからも僕の女として、\nそしてマシナの兵士として戦ってもらうよ"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "はい、小茂田様に全て従います\nこれからも私のことを使ってください"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "こうして有理紗は小茂田の女に堕とされてしまうのだった…"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "小茂田敗北エンド"
- },
+   "text": "来，过来"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "小茂田坐到椅子上，露出了勃起的阴茎。"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "那我进去了"
+  },
+  {
+   "type": "cg",
+   "key": "HC12-2"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "",
+   "effect": "flash"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "说着，有理纱以抱住小茂田的姿势插了进去。"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "啊啊…这就是小茂田大人的…"
+  },
+  {
+   "type": "say",
+   "speaker": "小茂田",
+   "text": "怎么样？被插入的感觉如何？"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "能感受到小茂田大人，我非常幸福"
+  },
+  {
+   "type": "say",
+   "speaker": "小茂田",
+   "text": "那就好，那我们动起来吧"
+  },
+  {
+   "type": "cg",
+   "key": "HC12-3"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "啪啪啪啪"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "啊嗯……啊啊嗯……"
+  },
+  {
+   "type": "say",
+   "speaker": "小茂田",
+   "text": "成为我的女人是什么感觉？"
+  },
+  {
+   "type": "cg",
+   "key": "HC12-4"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "是，非常幸福♡"
+  },
+  {
+   "type": "say",
+   "speaker": "小茂田",
+   "text": "今后也继续做我的女人吧？"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "当然了♡\n有理纱永远属于小茂田大人♡"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "啊嘻……好棒！再、再激烈一些"
+  },
+  {
+   "type": "say",
+   "speaker": "小茂田",
+   "text": "知道了\n来，这样如何"
+  },
+  {
+   "type": "cg",
+   "key": "HC12-5"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "动作变得激烈起来。"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "啊啊嗯好舒服，小茂田大人，我爱您"
+  },
+  {
+   "type": "say",
+   "speaker": "小茂田",
+   "text": "我也爱着有理纱哦"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "太高兴了，小茂田大人！"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "小茂田大人小茂田大人！我…快要去了…"
+  },
+  {
+   "type": "say",
+   "speaker": "小茂田",
+   "text": "我也差不多要射了…"
+  },
+  {
+   "type": "say",
+   "speaker": "小茂田",
+   "text": "啊啊，要射了"
+  },
+  {
+   "type": "cg",
+   "key": "HC12-6"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "",
+   "effect": "flash"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "",
+   "effect": "flash"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "噗噗噗——"
+  },
+  {
+   "type": "cg",
+   "key": "HC12-7"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "啊啊，小茂田大人的精液进到我的身体里…"
+  },
+  {
+   "type": "say",
+   "speaker": "小茂田",
+   "text": "呼……真不错啊有理纱酱……"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "谢谢您，小茂田大人♡"
+  },
+  {
+   "type": "say",
+   "speaker": "小茂田",
+   "text": "今后也要作为我的女人，\n以及马希纳的士兵而战哦"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "是，我一切都服从小茂田大人\n今后请继续使用我"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "就这样，有理纱堕落成了小茂田的女人…"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "小茂田败北结局"
+  },
  {
   "type": "end",
   "goto": "#vn-menu"
