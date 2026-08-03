bdbf5490c1d988ea93556f506ea1537fa796eb65
lumin <luminovoez@gmail.com>
2026-08-04 03:36:53 +0800

    translate(vn): isekai HA11/HA21/HA31 to Chinese

--- src/vn/scenes/isekai_HA11_13.ts
+++ src/vn/scenes/isekai_HA11_13.ts
@@ -32,159 +32,159 @@ export const isekai_HA11_13: VnScript = {
  {
   "type": "say",
   "speaker": "",
-  "text": "戦争はパワルが勝利した。"
- },
- {
-  "type": "cg",
-  "key": "HA11-1"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "勝利おめでとうございます茶羅井様"
- },
- {
-  "type": "say",
-  "speaker": "茶羅井",
-  "text": "有理紗も頑張ってくれたからだよ"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "ああんっ、茶羅井様褒めてくれてありがとうございます"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "セックスしながら勝利を祝い合う二人。"
- },
- {
-  "type": "say",
-  "speaker": "茶羅井",
-  "text": "有理紗が俺の女になってくれて良かったよ"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "お礼を言うのは私の方です"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "茶羅井様の女にしてもらえたから今こんなに幸せなんです！"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "もう茶羅井様のいない世界なんて考えられません！"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "茶羅井様愛してます！一生離れません！！"
- },
- {
-  "type": "say",
-  "speaker": "茶羅井",
-  "text": "(魅了の魔法は凄いな…\n 敵対していた女が一瞬でここまで依存するようになるんだもんな)"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "茶羅井様気持ち良いですか？"
- },
- {
-  "type": "say",
-  "speaker": "茶羅井",
-  "text": "ああ、良いぞ"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "ああんっ、嬉しいです！\nもっと気持ち良くなってください！"
- },
- {
-  "type": "say",
-  "speaker": "茶羅井",
-  "text": "ああ"
- },
- {
-  "type": "cg",
-  "key": "HA11-2"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "速度を上げる二人。"
- },
- {
-  "type": "say",
-  "speaker": "茶羅井",
-  "text": "ああ･･･イきそうだ、中に出すぞ！"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "はい！来てください！私の中を茶羅井様で満たしてください！"
- },
- {
-  "type": "cg",
-  "key": "HA11-3"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "",
-  "effect": "flash"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "ドピュッドピュー"
- },
- {
-  "type": "cg",
-  "key": "HA11-4"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "ああ･･･出てる･･･\n茶羅井様の精液が中に･･･嬉しいです！"
- },
- {
-  "type": "say",
-  "speaker": "茶羅井",
-  "text": "気持ち良かったよ"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "ありがとうございます茶羅井様\n一生お側でお仕えいたします"
- },
- {
-  "type": "cg",
-  "key": "HA11-5"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "んっ"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "キスをする二人。"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "こうして有理紗は茶羅井の女として過ごすのだった。"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "茶羅井敗北エンド"
- },
+   "text": "战争以帕尔卫的胜利告终。"
+  },
+  {
+   "type": "cg",
+   "key": "HA11-1"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "恭喜您获胜，茶罗井大人"
+  },
+  {
+   "type": "say",
+   "speaker": "茶罗井",
+   "text": "这也是因为有理纱拼命努力了啊"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "啊啊嗯，谢谢茶罗井大人夸奖"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "两人一边做爱，一边庆祝着胜利。"
+  },
+  {
+   "type": "say",
+   "speaker": "茶罗井",
+   "text": "有理纱能成为我的女人，真是太好了"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "该道谢的是我才对"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "正因为成了茶罗井大人的女人，我现在才这么幸福！"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "我已经无法想象没有茶罗井大人的世界了！"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "茶罗井大人，我爱您！这辈子都不会离开您！！"
+  },
+  {
+   "type": "say",
+   "speaker": "茶罗井",
+   "text": "（魅惑魔法真是厉害…\n 敌对的女人一瞬间就依赖到这种程度了）"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "茶罗井大人，舒服吗？"
+  },
+  {
+   "type": "say",
+   "speaker": "茶罗井",
+   "text": "啊，很棒"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "啊啊嗯，好高兴！\n请变得更舒服吧！"
+  },
+  {
+   "type": "say",
+   "speaker": "茶罗井",
+   "text": "啊啊"
+  },
+  {
+   "type": "cg",
+   "key": "HA11-2"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "两人加快了速度。"
+  },
+  {
+   "type": "say",
+   "speaker": "茶罗井",
+   "text": "啊啊…要去了，射在里面！"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "是！请来吧！请用茶罗井大人的精液填满我的身体！"
+  },
+  {
+   "type": "cg",
+   "key": "HA11-3"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "",
+   "effect": "flash"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "噗噗——噗——"
+  },
+  {
+   "type": "cg",
+   "key": "HA11-4"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "啊啊…出来了…\n茶罗井大人的精液进到里面…好高兴！"
+  },
+  {
+   "type": "say",
+   "speaker": "茶罗井",
+   "text": "很舒服哦"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "谢谢您，茶罗井大人\n我这一生都会在您身边侍奉您"
+  },
+  {
+   "type": "cg",
+   "key": "HA11-5"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "唔"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "两人接吻了。"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "就这样，有理纱作为茶罗井的女人生活着。"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "茶罗井败北结局"
+  },
  {
   "type": "end",
   "goto": "#vn-menu"
--- src/vn/scenes/isekai_HA21_14.ts
+++ src/vn/scenes/isekai_HA21_14.ts
@@ -80,391 +80,391 @@ export const isekai_HA21_14: VnScript = {
  {
   "type": "say",
   "speaker": "詩保",
-  "text": "身体が動かない…"
- },
- {
-  "type": "say",
-  "speaker": "小茂田",
-  "text": "デュフフ、詩保ちゃんようこそマシナ国へ"
- },
- {
-  "type": "say",
-  "speaker": "詩保",
-  "text": "その声、小茂田さん…？"
- },
- {
-  "type": "say",
-  "speaker": "小茂田",
-  "text": "そうだよ覚えててくれたんだ嬉しいなあ"
- },
- {
-  "type": "say",
-  "speaker": "詩保",
-  "text": "私をどうする気ですか？"
- },
- {
-  "type": "say",
-  "speaker": "小茂田",
-  "text": "それはね…僕の言うこと何でも聞いてくれる\n理想の女の子になってもらうんだよ"
- },
- {
-  "type": "cg",
-  "key": "HA21-2"
- },
- {
-  "type": "say",
-  "speaker": "詩保",
-  "text": "そんなのになるわけがありません！"
- },
- {
-  "type": "say",
-  "speaker": "小茂田",
-  "text": "そうだね･･･\n今のままだと僕のものになんて絶対になってくれないよね･･･"
- },
- {
-  "type": "say",
-  "speaker": "小茂田",
-  "text": "だから今付けている洗脳装置の出番ってわけさ"
- },
- {
-  "type": "cg",
-  "key": "HA21-3"
- },
- {
-  "type": "say",
-  "speaker": "詩保",
-  "text": "洗脳…装置…？"
- },
- {
-  "type": "say",
-  "speaker": "小茂田",
-  "text": "うん、僕の能力で作った機械の一つさ"
- },
- {
-  "type": "say",
-  "speaker": "小茂田",
-  "text": "これで詩保ちゃんを洗脳して僕の女にする！"
- },
- {
-  "type": "say",
-  "speaker": "小茂田",
-  "text": "ゆくゆくは有理紗ちゃんも僕の女にするつもりさ"
- },
- {
-  "type": "say",
-  "speaker": "詩保",
-  "text": "人の心を無理やり書き換えるなんて、\nそんな非人道的な行為はやめてください！"
- },
- {
-  "type": "say",
-  "speaker": "小茂田",
-  "text": "嫌だね、じゃあ始めるよ"
- },
- {
-  "type": "cg",
-  "key": "HA21-4"
- },
- {
-  "type": "say",
-  "speaker": "詩保",
-  "text": "ああああああ頭が痛いいいいいいいい"
- },
- {
-  "type": "say",
-  "speaker": "小茂田",
-  "text": "脳に電気でダメージを与えて、そこに僕の考えたデータを流し込む"
- },
- {
-  "type": "say",
-  "speaker": "小茂田",
-  "text": "そうすることで僕の理想データを\n植え付けていくことができるんだよ"
- },
- {
-  "type": "say",
-  "speaker": "詩保",
-  "text": "痛いいいいい痛いですうううううう"
- },
- {
-  "type": "say",
-  "speaker": "小茂田",
-  "text": "あれ？全然洗脳が進まない…"
- },
- {
-  "type": "say",
-  "speaker": "詩保",
-  "text": "あががががががが"
- },
- {
-  "type": "say",
-  "speaker": "小茂田",
-  "text": "もしかして抵抗している？\n確か詩保ちゃんの能力は回復だったよね･･･"
- },
- {
-  "type": "say",
-  "speaker": "小茂田",
-  "text": "まさか脳へのダメージも即座に回復している！？"
- },
- {
-  "type": "cg",
-  "key": "HA21-5"
- },
- {
-  "type": "say",
-  "speaker": "詩保",
-  "text": "私は…あなたのものになんかならないいいいいいい"
- },
- {
-  "type": "say",
-  "speaker": "小茂田",
-  "text": "ああそうかよ！そこまで僕のものになりたくないのかよ！！"
- },
- {
-  "type": "cg",
-  "key": "HA21-6"
- },
- {
-  "type": "cg",
-  "key": "HA21-6"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "洗脳装置が停止する。"
- },
- {
-  "type": "say",
-  "speaker": "詩保",
-  "text": "はあ…はあ…わかってくれたのですか…？"
- },
- {
-  "type": "say",
-  "speaker": "小茂田",
-  "text": "もういいよ、君の心は諦めるよ\n身体だけ使わせてもらうから"
- },
- {
-  "type": "say",
-  "speaker": "詩保",
-  "text": "何を言って…"
- },
- {
-  "type": "cg",
-  "key": "HA22-1"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "詩保に付けられている装置のバイザーが再び起動する。"
- },
- {
-  "type": "say",
-  "speaker": "詩保",
-  "text": "次は何ですか！？"
- },
- {
-  "type": "cg",
-  "key": "HA22-2"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "拘束が解除される。"
- },
- {
-  "type": "say",
-  "speaker": "詩保",
-  "text": "どうして･･･身体が動かないです…"
- },
- {
-  "type": "say",
-  "speaker": "小茂田",
-  "text": "そのバイザーから特殊な電気信号を送ってるんだよ"
- },
- {
-  "type": "say",
-  "speaker": "小茂田",
-  "text": "今の詩保ちゃんはそのバイザーからの指示でしか\n身体を動かせないんだ"
- },
- {
-  "type": "say",
-  "speaker": "詩保",
-  "text": "そんな…そんなことがあるわけ…"
- },
- {
-  "type": "say",
-  "speaker": "小茂田",
-  "text": "じゃあほら"
- },
- {
-  "type": "cg",
-  "key": "HA22-3"
- },
- {
-  "type": "cg",
-  "key": "HA22-3"
- },
- {
-  "type": "say",
-  "speaker": "詩保",
-  "text": "えっ、手が勝手に"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "詩保の手が勝手に胸へと向かう。"
- },
- {
-  "type": "say",
-  "speaker": "詩保",
-  "text": "きゃああ、何をさせるのですか！？"
- },
- {
-  "type": "say",
-  "speaker": "小茂田",
-  "text": "ほら、左手も"
- },
- {
-  "type": "cg",
-  "key": "HA22-4"
- },
- {
-  "type": "cg",
-  "key": "HA22-4"
- },
- {
-  "type": "say",
-  "speaker": "詩保",
-  "text": "いやああ手が勝手にいいいい"
- },
- {
-  "type": "say",
-  "speaker": "小茂田",
-  "text": "いいねいいね、エッチだよ"
- },
- {
-  "type": "cg",
-  "key": "HA22-5"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "その姿はオナニーをしているようにしか見えなかった。"
- },
- {
-  "type": "say",
-  "speaker": "詩保",
-  "text": "お願いです、こんなことやめてください"
- },
- {
-  "type": "say",
-  "speaker": "小茂田",
-  "text": "ダメだよ\n僕の洗脳を拒んだことを後悔させてあげる"
- },
- {
-  "type": "say",
-  "speaker": "小茂田",
-  "text": "イクまで…\nいや、イッても僕が良いと言うまでオナニーを続けるんだ"
- },
- {
-  "type": "cg",
-  "key": "HA22-6"
- },
- {
-  "type": "say",
-  "speaker": "詩保",
-  "text": "いやあああああ、お願いです、やめてください！！"
- },
- {
-  "type": "say",
-  "speaker": "詩保",
-  "text": "ああんっ、手が勝手にぃ"
- },
- {
-  "type": "say",
-  "speaker": "詩保",
-  "text": "でも、こんな無理やりやらされて気持ち良くなんてならないです！"
- },
- {
-  "type": "say",
-  "speaker": "小茂田",
-  "text": "そっか･･･でも僕が命令すればすぐにイクと思うよ"
- },
- {
-  "type": "say",
-  "speaker": "小茂田",
-  "text": "ほらイケ！"
- },
- {
-  "type": "cg",
-  "key": "HA22-7"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "",
-  "effect": "flash"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "絶頂してしまう詩保。"
- },
- {
-  "type": "cg",
-  "key": "HA22-8"
- },
- {
-  "type": "say",
-  "speaker": "詩保",
-  "text": "そんなぁ、どうして私…"
- },
- {
-  "type": "say",
-  "speaker": "小茂田",
-  "text": "詩保ちゃんの身体は僕が支配しているんだよ\n絶頂すらもね"
- },
- {
-  "type": "say",
-  "speaker": "詩保",
-  "text": "嘘でしょ･･･"
- },
- {
-  "type": "cg",
-  "key": "HA22-9"
- },
- {
-  "type": "say",
-  "speaker": "詩保",
-  "text": "ああんっ"
- },
- {
-  "type": "say",
-  "speaker": "詩保",
-  "text": "イッたから、休ませてぇ"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "絶頂しようがお構いなしに詩保の手は動き続ける。"
- },
- {
-  "type": "say",
-  "speaker": "小茂田",
-  "text": "こんなんじゃ済まさないよ\nエッチなことだけじゃない、\n君がきっと嫌がるであろうこと、いっぱいさせてあげるからね"
- },
- {
-  "type": "cg",
-  "key": "HA22-10"
- },
- {
-  "type": "say",
-  "speaker": "詩保",
-  "text": "いやぁ…もう許してください…"
- },
- {
-  "type": "say",
-  "speaker": "詩保",
-  "text": "(有理紗さん…助けて…)"
- },
+   "text": "身体动不了了…"
+  },
+  {
+   "type": "say",
+   "speaker": "小茂田",
+   "text": "嘿嘻嘻，诗保酱，欢迎来到马希纳国"
+  },
+  {
+   "type": "say",
+   "speaker": "诗保",
+   "text": "那个声音…是小茂田先生…？"
+  },
+  {
+   "type": "say",
+   "speaker": "小茂田",
+   "text": "没错，你还记得我啊，真高兴"
+  },
+  {
+   "type": "say",
+   "speaker": "诗保",
+   "text": "你想对我做什么？"
+  },
+  {
+   "type": "say",
+   "speaker": "小茂田",
+   "text": "这个嘛…我要让你成为\n我说什么都会听的理想中的女孩子"
+  },
+  {
+   "type": "cg",
+   "key": "HA21-2"
+  },
+  {
+   "type": "say",
+   "speaker": "诗保",
+   "text": "我才不会变成那样！"
+  },
+  {
+   "type": "say",
+   "speaker": "小茂田",
+   "text": "是啊…\n照现在这样，你绝对不可能成为我的东西…"
+  },
+  {
+   "type": "say",
+   "speaker": "小茂田",
+   "text": "所以就要靠现在戴着的这台洗脑装置了"
+  },
+  {
+   "type": "cg",
+   "key": "HA21-3"
+  },
+  {
+   "type": "say",
+   "speaker": "诗保",
+   "text": "洗脑…装置…？"
+  },
+  {
+   "type": "say",
+   "speaker": "小茂田",
+   "text": "嗯，是用我的能力制造的机器之一"
+  },
+  {
+   "type": "say",
+   "speaker": "小茂田",
+   "text": "用这个把诗保酱洗脑，让你成为我的女人！"
+  },
+  {
+   "type": "say",
+   "speaker": "小茂田",
+   "text": "将来我还打算把有理纱酱也变成我的女人"
+  },
+  {
+   "type": "say",
+   "speaker": "诗保",
+   "text": "强行改写人心这种\n不人道的行为，请住手！"
+  },
+  {
+   "type": "say",
+   "speaker": "小茂田",
+   "text": "我偏不要，那就开始了"
+  },
+  {
+   "type": "cg",
+   "key": "HA21-4"
+  },
+  {
+   "type": "say",
+   "speaker": "诗保",
+   "text": "啊啊啊啊啊头好痛啊啊啊啊啊啊"
+  },
+  {
+   "type": "say",
+   "speaker": "小茂田",
+   "text": "用电击对大脑造成损伤，再把我设计好的数据灌进去"
+  },
+  {
+   "type": "say",
+   "speaker": "小茂田",
+   "text": "这样一来，就能把理想的数据\n植入进去了"
+  },
+  {
+   "type": "say",
+   "speaker": "诗保",
+   "text": "好痛啊啊啊好痛啊啊啊啊啊啊"
+  },
+  {
+   "type": "say",
+   "speaker": "小茂田",
+   "text": "咦？洗脑完全没有进展…"
+  },
+  {
+   "type": "say",
+   "speaker": "诗保",
+   "text": "啊嘎嘎嘎嘎嘎嘎"
+  },
+  {
+   "type": "say",
+   "speaker": "小茂田",
+   "text": "难道你在抵抗？\n我记得诗保酱的能力是回复吧…"
+  },
+  {
+   "type": "say",
+   "speaker": "小茂田",
+   "text": "难道连大脑的损伤也能瞬间回复？！"
+  },
+  {
+   "type": "cg",
+   "key": "HA21-5"
+  },
+  {
+   "type": "say",
+   "speaker": "诗保",
+   "text": "我…绝不会成为你的东西啊啊啊啊啊"
+  },
+  {
+   "type": "say",
+   "speaker": "小茂田",
+   "text": "是吗好吧！你就那么不想成为我的东西吗！！"
+  },
+  {
+   "type": "cg",
+   "key": "HA21-6"
+  },
+  {
+   "type": "cg",
+   "key": "HA21-6"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "洗脑装置停止了。"
+  },
+  {
+   "type": "say",
+   "speaker": "诗保",
+   "text": "呼…呼…你放弃了吗…？"
+  },
+  {
+   "type": "say",
+   "speaker": "小茂田",
+   "text": "算了，你的心我不要了\n就让我用你的身体吧"
+  },
+  {
+   "type": "say",
+   "speaker": "诗保",
+   "text": "你在说什么…"
+  },
+  {
+   "type": "cg",
+   "key": "HA22-1"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "诗保戴着的装置的护目镜再次启动。"
+  },
+  {
+   "type": "say",
+   "speaker": "诗保",
+   "text": "接下来要做什么！？"
+  },
+  {
+   "type": "cg",
+   "key": "HA22-2"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "束缚被解开了。"
+  },
+  {
+   "type": "say",
+   "speaker": "诗保",
+   "text": "为什么…身体动不了了…"
+  },
+  {
+   "type": "say",
+   "speaker": "小茂田",
+   "text": "那个护目镜会发送特殊的电信号"
+  },
+  {
+   "type": "say",
+   "speaker": "小茂田",
+   "text": "现在的诗保酱只能听从护目镜的指示\n来活动身体"
+  },
+  {
+   "type": "say",
+   "speaker": "诗保",
+   "text": "怎么会…怎么可能有这种事…"
+  },
+  {
+   "type": "say",
+   "speaker": "小茂田",
+   "text": "那就看好了"
+  },
+  {
+   "type": "cg",
+   "key": "HA22-3"
+  },
+  {
+   "type": "cg",
+   "key": "HA22-3"
+  },
+  {
+   "type": "say",
+   "speaker": "诗保",
+   "text": "诶，手自己动起来了"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "诗保的手自己伸向了胸部。"
+  },
+  {
+   "type": "say",
+   "speaker": "诗保",
+   "text": "呀啊啊，你要让我做什么！？"
+  },
+  {
+   "type": "say",
+   "speaker": "小茂田",
+   "text": "来，左手也是"
+  },
+  {
+   "type": "cg",
+   "key": "HA22-4"
+  },
+  {
+   "type": "cg",
+   "key": "HA22-4"
+  },
+  {
+   "type": "say",
+   "speaker": "诗保",
+   "text": "不要啊手自己动起来啊啊啊"
+  },
+  {
+   "type": "say",
+   "speaker": "小茂田",
+   "text": "不错不错，很色情哦"
+  },
+  {
+   "type": "cg",
+   "key": "HA22-5"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "那副模样，看起来就像是在自慰。"
+  },
+  {
+   "type": "say",
+   "speaker": "诗保",
+   "text": "求求你了，快停下来"
+  },
+  {
+   "type": "say",
+   "speaker": "小茂田",
+   "text": "不行哦\n我要让你为拒绝我的洗脑而后悔"
+  },
+  {
+   "type": "say",
+   "speaker": "小茂田",
+   "text": "一直到高潮…\n不，即使高潮了，也要持续自慰到我满意为止"
+  },
+  {
+   "type": "cg",
+   "key": "HA22-6"
+  },
+  {
+   "type": "say",
+   "speaker": "诗保",
+   "text": "不要啊啊啊啊，求你了，住手！！"
+  },
+  {
+   "type": "say",
+   "speaker": "诗保",
+   "text": "啊啊嗯，手自己动了起来"
+  },
+  {
+   "type": "say",
+   "speaker": "诗保",
+   "text": "但是，被这样强迫，我才不会觉得舒服！"
+  },
+  {
+   "type": "say",
+   "speaker": "小茂田",
+   "text": "是吗…不过我一命令，你马上就会高潮的"
+  },
+  {
+   "type": "say",
+   "speaker": "小茂田",
+   "text": "来，高潮吧！"
+  },
+  {
+   "type": "cg",
+   "key": "HA22-7"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "",
+   "effect": "flash"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "诗保到达了高潮。"
+  },
+  {
+   "type": "cg",
+   "key": "HA22-8"
+  },
+  {
+   "type": "say",
+   "speaker": "诗保",
+   "text": "怎么会，为什么我…"
+  },
+  {
+   "type": "say",
+   "speaker": "小茂田",
+   "text": "诗保酱的身体由我支配\n连高潮也是"
+  },
+  {
+   "type": "say",
+   "speaker": "诗保",
+   "text": "骗人的吧…"
+  },
+  {
+   "type": "cg",
+   "key": "HA22-9"
+  },
+  {
+   "type": "say",
+   "speaker": "诗保",
+   "text": "啊啊嗯"
+  },
+  {
+   "type": "say",
+   "speaker": "诗保",
+   "text": "已经高潮了，让我歇歇嘛"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "即使已经高潮，诗保的手依然不停动作着。"
+  },
+  {
+   "type": "say",
+   "speaker": "小茂田",
+   "text": "不会就这么算了的\n不只是色情的事，\n凡是会让你讨厌的事，我都会让你做很多"
+  },
+  {
+   "type": "cg",
+   "key": "HA22-10"
+  },
+  {
+   "type": "say",
+   "speaker": "诗保",
+   "text": "不要啊…放过我吧…"
+  },
+  {
+   "type": "say",
+   "speaker": "诗保",
+   "text": "（有理纱小姐…救救我…）"
+  },
  {
   "type": "end",
   "goto": "#vn-menu"
