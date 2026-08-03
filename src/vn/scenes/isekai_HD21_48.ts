938577cf45b0e7b90a02634c7727431afd80d158
lumin <luminovoez@gmail.com>
2026-08-04 05:23:56 +0800

    translate(vn): isekai HD21 to Chinese

--- src/vn/scenes/isekai_HD21_48.ts
+++ src/vn/scenes/isekai_HD21_48.ts
@@ -72,368 +72,368 @@ export const isekai_HD21_48: VnScript = {
  {
   "type": "say",
   "speaker": "",
-  "text": "こうして狂信者によるセックス指導が始まった。"
- },
- {
-  "type": "cg",
-  "key": "HD21-1"
- },
- {
-  "type": "say",
-  "speaker": "狂信者",
-  "text": "ではまずはセックスに不要な処女は捨てましょうね"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "はいお願いします！"
- },
- {
-  "type": "cg",
-  "key": "HD21-2"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "",
-  "effect": "flash"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "そう言って挿入する狂信者。"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "痛っ…"
- },
- {
-  "type": "say",
-  "speaker": "狂信者",
-  "text": "まあこうなりますよね\nこんなことでは男性を誘惑することはできませんからね"
- },
- {
-  "type": "say",
-  "speaker": "狂信者",
-  "text": "処女を捨てられて良かったでしょう"
- },
- {
-  "type": "cg",
-  "key": "HD21-3"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "はい！あなたには感謝しかないです！\n私の処女を捨ててくれてありがとうございます！！"
- },
- {
-  "type": "say",
-  "speaker": "狂信者",
-  "text": "初めてですし、まずは慣らすためにゆっくりと動きますよ"
- },
- {
-  "type": "cg",
-  "key": "HD21-4"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "そう言いゆっくりとピストンを始める。"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "うう…やっぱり痛いです"
- },
- {
-  "type": "say",
-  "speaker": "狂信者",
-  "text": "我慢するのです。時期に良くなってきますから"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "そう言いしばらくの間はゆっくりとしたピストンが続く。"
- },
- {
-  "type": "cg",
-  "key": "HD21-5"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "あの、痛くなくなってきたかもしれません"
- },
- {
-  "type": "say",
-  "speaker": "狂信者",
-  "text": "それは良かったです\nではこれからは男性を喜ばせるセックスの指導をしていきますよ"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "はい、よろしくお願いします"
- },
- {
-  "type": "cg",
-  "key": "HD21-6"
- },
- {
-  "type": "cg",
-  "key": "HD21-6"
- },
- {
-  "type": "say",
-  "speaker": "狂信者",
-  "text": "では挿入からやり直します"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "そう言いペニスを抜く狂信者。"
- },
- {
-  "type": "say",
-  "speaker": "狂信者",
-  "text": "入れてもらう時も、男性が喜ぶような言動を心がけるのです"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "わかりました"
- },
- {
-  "type": "say",
-  "speaker": "狂信者",
-  "text": "ほら\n『私のおマンコにそのたくましいおチンポを入れてください』\nと言いなさい"
- },
- {
-  "type": "cg",
-  "key": "HD21-7"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "はい！\n私のおマンコにそのたくましいおチンポを入れてください！"
- },
- {
-  "type": "say",
-  "speaker": "狂信者",
-  "text": "良いでしょう、入れますよ"
- },
- {
-  "type": "cg",
-  "key": "HD21-8"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "再び挿入される有理紗。"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "んんっ"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "まだ慣れないのか、抵抗感のある反応をする有理紗。"
- },
- {
-  "type": "say",
-  "speaker": "狂信者",
-  "text": "違いますよ、そこは嘘でも喘いでください"
- },
- {
-  "type": "say",
-  "speaker": "狂信者",
-  "text": "チンポを入れたことで気持ち良くなったと思ってもらわないと"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "すみません"
- },
- {
-  "type": "say",
-  "speaker": "狂信者",
-  "text": "では挿入からやり直しますよ"
- },
- {
-  "type": "cg",
-  "key": "HD21-9"
- },
- {
-  "type": "cg",
-  "key": "HD21-9"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "ペニスが抜かれる。"
- },
- {
-  "type": "cg",
-  "key": "HD21-10"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "私のおマンコにそのたくましいおチンポを入れてください！"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "再び入れてもらうための言葉を発する有理紗。"
- },
- {
-  "type": "cg",
-  "key": "HD21-11"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "ああんっ、あなたのたくましいおチンポが入ってきてます"
- },
- {
-  "type": "say",
-  "speaker": "狂信者",
-  "text": "良いですね、その調子です\nでは動きますよ"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "はい！"
- },
- {
-  "type": "cg",
-  "key": "HD21-12"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "腰を動かす狂信者。"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "んんっ、あんっ"
- },
- {
-  "type": "say",
-  "speaker": "狂信者",
-  "text": "まだまだ甘いですよ\nもっと良い反応をするように頑張ってください"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "はい、頑張ります！"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "その後もセックス指導は続く。"
- },
- {
-  "type": "cg",
-  "key": "HD21-13"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "ピストンも徐々に早くなっていく。"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "ああんっ、良いっ、あなたのおチンポ気持ち良いです！\nもっともっと動いてください！"
- },
- {
-  "type": "say",
-  "speaker": "狂信者",
-  "text": "かなり良くなりましたね"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "ありがとうございます"
- },
- {
-  "type": "say",
-  "speaker": "狂信者",
-  "text": "ああ、そろそろ出そうです\n出してもらう時はどう言うんでしたか？"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "はい、中に…中に出してください！！"
- },
- {
-  "type": "say",
-  "speaker": "狂信者",
-  "text": "良いですね\nでは出しますよ"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "はいっ！来てください！あなたの精子を私の中に出してください！"
- },
- {
-  "type": "say",
-  "speaker": "狂信者",
-  "text": "うっ"
- },
- {
-  "type": "cg",
-  "key": "HD21-14"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "",
-  "effect": "flash"
- },
- {
-  "type": "say",
-  "speaker": "",
-  "text": "中に出される有理紗。"
- },
- {
-  "type": "cg",
-  "key": "HD21-15"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "ああっ、出てる、熱いのがドクドクと注がれてます……"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "はぁはぁ……"
- },
- {
-  "type": "say",
-  "speaker": "狂信者",
-  "text": "いかがでしたか？"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "ご指導ありがとうございます\nこれで誰に抱かれても大丈夫だと思います"
- },
- {
-  "type": "say",
-  "speaker": "狂信者",
-  "text": "そうですか\nではこれからは共に勧誘活動頑張りましょう"
- },
- {
-  "type": "say",
-  "speaker": "有理紗",
-  "text": "はい"
- },
+   "text": "就这样，狂信者的性爱指导开始了。"
+  },
+  {
+   "type": "cg",
+   "key": "HD21-1"
+  },
+  {
+   "type": "say",
+   "speaker": "狂信者",
+   "text": "那么首先，把做爱不需要的处女之身舍弃掉吧"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "是，拜托了！"
+  },
+  {
+   "type": "cg",
+   "key": "HD21-2"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "",
+   "effect": "flash"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "说着，狂信者插了进去。"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "好痛…"
+  },
+  {
+   "type": "say",
+   "speaker": "狂信者",
+   "text": "嗯，果然会这样\n这样可是无法诱惑男性的"
+  },
+  {
+   "type": "say",
+   "speaker": "狂信者",
+   "text": "能舍弃处女之身，很好吧"
+  },
+  {
+   "type": "cg",
+   "key": "HD21-3"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "是！我对你只有感谢！\n谢谢你帮我舍弃了处女之身！！"
+  },
+  {
+   "type": "say",
+   "speaker": "狂信者",
+   "text": "因为是第一次，先慢慢动着让你适应"
+  },
+  {
+   "type": "cg",
+   "key": "HD21-4"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "说着，开始了缓慢的抽插。"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "呜呜…果然还是好痛"
+  },
+  {
+   "type": "say",
+   "speaker": "狂信者",
+   "text": "忍一忍。过一会儿就会好起来的"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "说着，缓慢的抽插持续了一阵子。"
+  },
+  {
+   "type": "cg",
+   "key": "HD21-5"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "那个，好像不那么痛了"
+  },
+  {
+   "type": "say",
+   "speaker": "狂信者",
+   "text": "那就好\n接下来就指导你如何取悦男性的性爱技巧"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "是，请多指教"
+  },
+  {
+   "type": "cg",
+   "key": "HD21-6"
+  },
+  {
+   "type": "cg",
+   "key": "HD21-6"
+  },
+  {
+   "type": "say",
+   "speaker": "狂信者",
+   "text": "那就从插入重新开始"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "说着，狂信者拔出了阴茎。"
+  },
+  {
+   "type": "say",
+   "speaker": "狂信者",
+   "text": "被插入的时候，也要注意说些让男性高兴的话"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "明白了"
+  },
+  {
+   "type": "say",
+   "speaker": "狂信者",
+   "text": "来\n就说『请把您那雄伟的肉棒放进我的小穴里』"
+  },
+  {
+   "type": "cg",
+   "key": "HD21-7"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "是！\n请把您那雄伟的肉棒放进我的小穴里！"
+  },
+  {
+   "type": "say",
+   "speaker": "狂信者",
+   "text": "很好，我要进去了"
+  },
+  {
+   "type": "cg",
+   "key": "HD21-8"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "有理纱再次被插入。"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "唔嗯"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "还没适应的有理纱，做出了带有抵抗感的反应。"
+  },
+  {
+   "type": "say",
+   "speaker": "狂信者",
+   "text": "不对哦，那里要装出喘息的样子"
+  },
+  {
+   "type": "say",
+   "speaker": "狂信者",
+   "text": "得让对方觉得，被肉棒插入让你很舒服才行"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "对不起"
+  },
+  {
+   "type": "say",
+   "speaker": "狂信者",
+   "text": "那就从插入重新开始吧"
+  },
+  {
+   "type": "cg",
+   "key": "HD21-9"
+  },
+  {
+   "type": "cg",
+   "key": "HD21-9"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "阴茎被拔了出来。"
+  },
+  {
+   "type": "cg",
+   "key": "HD21-10"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "请把您那雄伟的肉棒放进我的小穴里！"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "有理纱再次说出了请求插入的话语。"
+  },
+  {
+   "type": "cg",
+   "key": "HD21-11"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "啊啊嗯，您那雄伟的肉棒进来了"
+  },
+  {
+   "type": "say",
+   "speaker": "狂信者",
+   "text": "不错，就这样\n那我要动了"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "是！"
+  },
+  {
+   "type": "cg",
+   "key": "HD21-12"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "狂信者扭动着腰部。"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "唔嗯，啊嗯"
+  },
+  {
+   "type": "say",
+   "speaker": "狂信者",
+   "text": "还差得远呢\n努力做出更好的反应吧"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "是，我会努力的！"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "之后，性爱指导仍在继续。"
+  },
+  {
+   "type": "cg",
+   "key": "HD21-13"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "抽插也逐渐变快。"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "啊啊嗯，好棒，你的肉棒好舒服！\n请再快一点！"
+  },
+  {
+   "type": "say",
+   "speaker": "狂信者",
+   "text": "进步很大啊"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "谢谢您"
+  },
+  {
+   "type": "say",
+   "speaker": "狂信者",
+   "text": "啊啊，差不多要射了\n被射的时候该说什么来着？"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "是，请射在里面…请射在身体里！！"
+  },
+  {
+   "type": "say",
+   "speaker": "狂信者",
+   "text": "很好\n那我要射了"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "是！请来吧！请把您的精子射进我的身体里！"
+  },
+  {
+   "type": "say",
+   "speaker": "狂信者",
+   "text": "唔"
+  },
+  {
+   "type": "cg",
+   "key": "HD21-14"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "",
+   "effect": "flash"
+  },
+  {
+   "type": "say",
+   "speaker": "",
+   "text": "精液被射进了有理纱体内。"
+  },
+  {
+   "type": "cg",
+   "key": "HD21-15"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "啊啊，出来了，灼热的液体咕嘟咕嘟地注入进来了……"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "呼啊呼啊……"
+  },
+  {
+   "type": "say",
+   "speaker": "狂信者",
+   "text": "感觉如何？"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "感谢您的指导\n这样无论被谁抱都没问题了"
+  },
+  {
+   "type": "say",
+   "speaker": "狂信者",
+   "text": "是吗\n那今后就一起努力进行传教活动吧"
+  },
+  {
+   "type": "say",
+   "speaker": "有理纱",
+   "text": "是"
+  },
  {
   "type": "end",
   "goto": "#vn-menu"
